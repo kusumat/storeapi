20f8241fff2255203173a4a6eb33c6eb7bf80406
Lakshmi Tatavarthi <nagavenkatash.tatav@hcl.com>
2022-11-21 12:30:13 +0530

    Pushing built changes

--- studioactions/desktop/AS_Button_e88479a0d564481e852d5bc2adf00ed3.js
+++ studioactions/desktop/AS_Button_e88479a0d564481e852d5bc2adf00ed3.js
@@ -1,3 +1,3 @@
 function AS_Button_e88479a0d564481e852d5bc2adf00ed3(eventobject) {
-return getItem.call(this);
+    return getItem.call(this);
 }
--- studioactions/desktop/AS_Button_ea25cfe64f334399a0aa3dc858cbda25.js
+++ studioactions/desktop/AS_Button_ea25cfe64f334399a0aa3dc858cbda25.js
@@ -1,3 +1,3 @@
 function AS_Button_ea25cfe64f334399a0aa3dc858cbda25(eventobject) {
-return removeItem.call(this);
+    return removeItem.call(this);
 }
